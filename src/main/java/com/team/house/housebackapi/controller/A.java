9abfb55a4a8f2d91ef38b29232c7a9dd69ccec0e
wjb <superich.wjb@163.com>
2020-03-05 12:08:08 +0800

    组员A编写

---
+++ src/main/java/com/team/house/housebackapi/controller/A.java
@@ -0,0 +1,14 @@
+package com.team.house.housebackapi.controller;
+
+/**
+ * @author 王建兵
+ * @Classname A
+ * @Description TODO
+ * @Date 2020/3/5 12:02
+ * @Created by Administrator
+ */
+public class A {
+    public void ss(){
+
+    }
+}
